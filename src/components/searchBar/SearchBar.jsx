2b2d999f2d7b809234e67cfaa6e12c16d5f7cb55
Sékou S. Seh <sehsekou98@gmail.com>
2025-02-14 17:41:42 +0100

    home

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import "./searchBar.scss";
 
+
+
+const types = ["buy", "rent"];
 function SearchBar() {
+  const [query,setQuerry] = useState({
+    type: "Buy",
+    location: "",
+    minPrice: 0,
+    maxPrice:0,
+})
+
+const switchType = (val) => {
+
+}
   return (
-    <div>
-      
-    </div>
+    <div className='searchBar'>
+      <div className='type'>
+        {types.map((type) => {
+          <button key={type} onClick={() =>switchType(types)}>
+            (type)
+          </button>
+})}
+      </div>
+        <form>
+          <input type="text"
+           name='location'
+           placeholder='City Location'
+           />
+          <input 
+          type="number"
+          name='mainPrice'
+          min={0}
+          max={1000000}
+          placeholder='Min Price'
+          />
+          <input 
+          type="number"
+          name='maxPrice'
+          min={0}
+          max={1000000}
+          placeholder='Max Price'
+          />
+          <button>
+            <img src="/search.png" alt="" />
+          </button>
+        </form>
+      </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
