ae29d816b72d9f2472e1f7bc68b793440a4b8ca4
Sekou S Seh <sehsekou98@gmail.com>
2025-06-03 15:40:13 +0200

    searchbar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,54 +1,55 @@
 import { useState } from "react";
 import "./searchBar.scss";
 
-
-
 const types = ["buy", "rent"];
+
 function SearchBar() {
-  const [query,setQuerry] = useState({
-    type: "Buy",
+  const [query, setQuery] = useState({
+    type: "buy",
     location: "",
     minPrice: 0,
-    maxPrice:0,
-})
+    maxPrice: 0,
+  });
 
-const switchType = (val) => {
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
 
-}
   return (
-    <div className='searchBar'>
-      <div className='type'>
-        {types.map((type) => {
-          <button key={type} onClick={() =>switchType(types)}>
-            (type)
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
           </button>
-})}
+        ))}
       </div>
-        <form>
-          <input type="text"
-           name='location'
-           placeholder='City Location'
-           />
-          <input 
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
           type="number"
-          name='mainPrice'
+          name="minPrice"
           min={0}
-          max={1000000}
-          placeholder='Min Price'
-          />
-          <input 
+          max={10000000}
+          placeholder="Min Price"
+        />
+        <input
           type="number"
-          name='maxPrice'
+          name="maxPrice"
           min={0}
-          max={1000000}
-          placeholder='Max Price'
-          />
-          <button>
-            <img src="/search.png" alt="" />
-          </button>
-        </form>
-      </div>
-  )
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
