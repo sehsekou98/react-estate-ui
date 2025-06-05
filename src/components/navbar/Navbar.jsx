d154575c0281c5dee89c6763617f5193b2449b27
Sekou S Seh <sehsekou98@gmail.com>
2025-06-05 20:41:07 +0200

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>J$J-Transport Service</span>
+          <span>G$G Transit Assistance Sarl</span>
         </a>
         <a href="/home">Home</a>
         <a href="/about">About</a>
