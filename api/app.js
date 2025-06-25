d16148634d846f6efa55a96060d2810a98b0fdf4
Sekou S Seh <sehsekou98@gmail.com>
2025-06-25 19:48:22 +0200

    create user

--- api/app.js
+++ api/app.js
@@ -4,9 +4,11 @@ import authRouth from "./routes/auth.route.js";
 
 const app = express();
 
+app.use(express.json())
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRouth);
 
-app.listen(8800, () => {
+app.listen(8080, () => {
 console.log("Server is running!");
 });
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,38 @@
-export const register = (req, res) => {
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js"; // Include `.js`
 
+export const register = async (req, res) => {
+const { username, email, password } = req.body;
+
+try {
+    
+// Hash password
+
+const hashedPassword = await bcrypt.hash(password, 10);
+console.log(hashedPassword);
+
+// CREATING A NEW USER
+
+const newUser = await prisma.user.create({
+    data: {
+        username,
+        email,
+        password: hashedPassword,
+    },
+});
+
+console.log(newUser);
+
+res.status(201).json({message:"User created successfully."});
+
+} catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create user."});
+    
 }
+
+
+};
 
 export const login = (req, res) => {
     
