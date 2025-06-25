8d81346c20cfdb99586ae7b7542dd191b351017f
Sekou S Seh <sehsekou98@gmail.com>
2025-06-25 17:30:35 +0200

    authapi

--- api/app.js
+++ api/app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import postRoute from "./routes/post.route.js";
+import authRouth from "./routes/auth.route.js";
 
 const app = express();
 
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRouth);
 
-app.listen(8080, () => {
+app.listen(8800, () => {
 console.log("Server is running!");
 });
