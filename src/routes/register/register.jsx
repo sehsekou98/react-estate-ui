01096024e2e7c4094377bb8ab6679afdbfb17c62
Sekou S Seh <sehsekou98@gmail.com>
2025-06-26 20:36:12 +0200

    update

--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,23 +1,25 @@
 import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
 
-  const [error,setError] = useState("")
+  const [error,setError] = useState("");
+  const [ isLoading, setIsLoading] = useState(false)
 
   const navigate = useNavigate();
 
   const handelSubmit = async (e) => {
   e.preventDefault();
+  setIsLoading(true);
   const formData = new FormData(e.target);
   const username = formData.get("username");
   const email = formData.get("email");
   const password = formData.get("password");
 
   try {
-    const res = await axios.post("http://localhost:8080/api/auth/register", {
+    const res = await apiRequest.post("/auth/register", {
       username,
       email,
       password,
@@ -25,6 +27,8 @@ function Register() {
     navigate("/login");
   } catch (error) {
     setError(error.response?.data?.message || "Registration failed");
+  } finally {
+    setIsLoading(false)
   }
 };
 
@@ -37,13 +41,13 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span> {error} </span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
       <div className="imgContainer">
-        <img src="/bg.png" alt="" />
+        <img src="/bg.png" alt="" /> 
       </div>
     </div>
   );
