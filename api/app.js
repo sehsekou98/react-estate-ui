82951db03e832394697bb418e5300b54ee9e5afd
Sekou S Seh <sehsekou98@gmail.com>
2025-06-27 20:00:10 +0200

    update

--- api/app.js
+++ api/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
-import authRouth from "./routes/auth.route.js";
+import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -11,7 +12,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
-app.use("/api/auth", authRouth);
+app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8080, () => {
     console.log("Server is running!");
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,14 +53,14 @@ export const login = async (req, res) => {
 
         // GENERATE COOKIE TOKEN AND SEND TO THE USER.
 
-        // res.setHeader("Set-Cookie", "test" + "myValue").json("success")
-
+        
 
         const age = 1000 * 60 * 60 * 24 * 7
 
 
         const token = jwt.sign({
-            id: user.id
+            id: user.id,
+            isAdmin: true,
         }, process.env.JWT_SECRET_KEY,
             { expiresIn: age });
 
